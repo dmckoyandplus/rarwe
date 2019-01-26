baadfce94754590408517a978e8ea57ce8845fa1
McKoy <mckoy@AndPluss-MacBook-Pro.local>
2019-01-26 18:48:27 -0500

    modified band params to handle api calls for a single band

--- app/models/band.js
+++ app/models/band.js
@@ -7,9 +7,5 @@ const { Model, attr, hasMany } = DS;
 export default Model.extend({
   name: attr('string'),
   description: attr('string'),
-  songs: hasMany(),
-
-  slug: computed('name', function(){
-    return dasherize(this.name);
-  })
+  songs: hasMany()
 });
